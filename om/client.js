15e63b726ecf59d6be69119c5ad51f8c7f4d19e8
Guy Royse <guy@guyroyse.com>
2022-03-08 11:23:00 -0500

    exposed Node Redis

--- om/client.js
+++ om/client.js
@@ -3,16 +3,16 @@ import { Client } from 'redis-om'
 /* Redis OM uses the client to talk to Redis. It uses
    Node Redis behind the scences. */
 
-const client = await new Client().open(process.env.REDIS_URL)
+// const client = await new Client().open(process.env.REDIS_URL)
 
 /* If you want to use Node Redis and Redis OM together,
    comment the code above and use the code below. */
 
-// import { createClient } from 'redis' 
+import { createClient } from 'redis' 
 
-// export const connection = createClient({ url: process.env.REDIS_URL })
-// await connection.connect()
+export const connection = createClient({ url: process.env.REDIS_URL })
+await connection.connect()
 
-// const client = await new Client().use(connection)
+const client = await new Client().use(connection)
 
 export default client
