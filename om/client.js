6a02eb0145588f2b6ab22b116c42ced5a06294fd
Guy Royse <guy@guyroyse.com>
2022-03-08 14:21:27 -0500

    updated comments in client for streams

--- om/client.js
+++ om/client.js
@@ -1,12 +1,14 @@
 import { Client } from 'redis-om'
 
 /* Redis OM uses the client to talk to Redis. It uses
-   Node Redis behind the scences. */
+   Node Redis behind the scenes. If you want to let
+   Redis OM handle all that, just use the commented
+   code below. */
 
 // const client = await new Client().open(process.env.REDIS_URL)
 
 /* If you want to use Node Redis and Redis OM together,
-   comment the code above and use the code below. */
+   keep the code below uncommented. */
 
 import { createClient } from 'redis' 
 
@@ -14,5 +16,7 @@ export const connection = createClient({ url: process.env.REDIS_URL })
 await connection.connect()
 
 const client = await new Client().use(connection)
+
+/* Regardless, keep this code. */
 
 export default client
